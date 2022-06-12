a61412f1bfb0a20411ab023e7b088f19b0dec043
Michael Kleemoff <michaelkleemoff2@gmail.com>
2022-06-12 10:16:24 -0400

    Add createPerson.

---
+++ index.js
@@ -0,0 +1,19 @@
+/** @format */
+
+function createPerson(name, age) {
+	return {
+		name,
+		age,
+		friends: [],
+	};
+}
+
+function addPersonAsFriend(person, other) {
+	person.friends.push(other);
+	return person;
+}
+
+module.exports = {
+	createPerson,
+	addPersonAsFriend,
+};
